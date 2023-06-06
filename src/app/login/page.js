032bff650bd0848b6af19fbacc499bbd74481d59
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-06-06 23:12:31 +0530

    learn routing

---
+++ src/app/login/page.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+const page = () => {
+    return (
+        <h1 className={styles.main}>Login Page</h1>
+    );
+}
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [name, setName] = useState("Nitesh")
+  const apple = (item) => {
+    name !== "Nitesh" ? setName("Nitesh") : setName("Nancy")
+  }
+
   return (
     <main className={styles.main}>
-      <h1>Home Page</h1>
+      <h1>{name}</h1>
+      <button onClick={() => apple()}>I Love You</button>
     </main>
   )
 }
